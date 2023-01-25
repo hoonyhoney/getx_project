c9db20aa5321cc199fe17af03ecfe624f22452e5
hoonyhoney <hoonyhoeny@gmail.com>
2023-01-25 23:08:23 +0900

    toNamed, argument 활용

--- lib/GetxNavigation/ScreenTwo.dart
+++ lib/GetxNavigation/ScreenTwo.dart
@@ -1,22 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ScreenTwo extends StatelessWidget {
+class ScreenTwo extends StatefulWidget {
+  @override
+  State<ScreenTwo> createState() => _ScreenTwoState();
+}
 
+class _ScreenTwoState extends State<ScreenTwo> {
+  late int radioVal = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Container(child: Column(
-        children: [
-          Text("스크린 TWO"),
-          RaisedButton(onPressed: (){
-            Get.back();
-          },
-          child:Text("뒤로가기"),),
-        ],
-      )),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Container(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 500,
+            ),
+            Text("스크린 TWO"),
+            RaisedButton(
+              onPressed: () {
+                Get.back(result: radioVal);
+              },
+              child: Text("뒤로가기"),
+            ),
+            Row(children: [
+              SizedBox(
+                width: 300,
+              ),
+              Radio(
+                groupValue: radioVal,
+                value: 0,
+                onChanged: (int? value) {
+                  setState(() {
+                    radioVal = value!;
+                  });
+                },
+              ),
+              Text('0'),
+            ]),
+            Row(
+              children: [
+                SizedBox(
+                  width: 300,
+                ),
+                Radio(
+                  groupValue: radioVal,
+                  value: 1,
+                  onChanged: (int? value) {
+                    setState(() {
+                      radioVal = value!;
+                    });
+                  },
+                ),
+                Text('1'),
+              ],
+            ),
+            Text(
+                Get.parameters['param']!,
+            ),            Text(
+                Get.parameters['id']!,
+            )
+          ],
+        )),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,18 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  int returnVal = 10;
+  String? message="";
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -19,33 +27,41 @@ class MyApp extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 100),
-          RaisedButton(onPressed: () {
-            Get.to(ScreenTwo());
-          },
+          RaisedButton(
+            onPressed: () {
+              Get.to(ScreenTwo());
+            },
             child: Text("ScreeTwo로이동"),
           ),
+          RaisedButton(
+              onPressed: () {
+                Get.offAll(ScreenTwo());
+              },
+              child: Text("기록삭제하고 이동")),
+          RaisedButton(
+            onPressed: () async {
+              final resp = await Get.to(ScreenTwo());
+              setState(() {
+                returnVal = resp;
+              });
+            },
+            child: Text("리턴값 받아오기"),
+          ),
+          Text('리턴값: $returnVal'),
+          RaisedButton(
+              onPressed: () {
+                Get.to(ScreenTwo(), arguments: message);
+              },
+              child: Text("argument보내기")),
           RaisedButton(onPressed: () {
-            Get.offAll(ScreenTwo());
-          },child:Text("기록삭제하고 이동")),
+            Get.to(ScreenTwo(),
+            transition: Transition.fadeIn);
+          },child:Text("transition")),
           RaisedButton(onPressed: () {
-
-          }),
-          RaisedButton(onPressed: () {
-
-          }),
-          RaisedButton(onPressed: () {
-
-          }),
-          RaisedButton(onPressed: () {
-
-          }),
-          RaisedButton(onPressed: () {
-
-          }),
-          RaisedButton(onPressed: () {
-
-          }),
-
+            Get.toNamed('/two/1234?id=id');
+          },child: Text("네임드라우트"),),
+          RaisedButton(onPressed: () {}),
+          RaisedButton(onPressed: () {}),
           RaisedButton(
             onPressed: () {
               Get.snackbar("새해인사", "새해복 많이 받으세요",
@@ -59,13 +75,16 @@ class MyApp extends StatelessWidget {
             },
             child: Text("dialog"),
           ),
-          RaisedButton(onPressed: () {
-
-          }),
+          RaisedButton(onPressed: () {}),
         ],
       ),
-    ));
+    ),
+      getPages: [
+        GetPage(
+          name: '/two/:param',
+          page:()=> ScreenTwo(),
+        ),
+      ],
+    );
   }
 }
-
-
